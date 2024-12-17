ea57605b332f11dc3f9582e8cf0e10b6a6eecfd8
unfold <studiovogue01@gmail.com>
2024-12-18 00:25:59 +0530

    CategoriesController removing @RequiresArgs

--- src/main/java/com/unfold/unfoldfit/controller/CategoriesController.java
+++ src/main/java/com/unfold/unfoldfit/controller/CategoriesController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping(value="/rest/unfold")
 public class CategoriesController {
 
